2c1c4fa2a4fe5ed7c9dffb83176b8e653ca51577
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-05-18 09:49:14 +0530

    Your 1 changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const jwt = require("jsonwebtoken");
 const upload = require("./multer");
 const fs = require("fs");
 const path = require("path");
-const express = require('express');
+
 const upload = require('./uploads'); 
 
 const {authenticateToken} = require("./utilies"); 
